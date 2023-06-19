746dc4358f3f08bdffa7843df5d50376d6942cea
zhu_zhibin <zhu_zhibin@outlook.com>
2023-06-19 22:40:16 +0800

    Initial commit

---
+++ src/main/java/com/zhuzimo/crack/CrackApplication.java
@@ -0,0 +1,13 @@
+package com.zhuzimo.crack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrackApplication.class, args);
+    }
+
+}
